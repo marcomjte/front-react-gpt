519c1fa0860b601f4c7d9de56ef07619fd6c997d
Marco Jiménez <marcojimenezmte@gmail.com>
2025-05-24 19:42:46 -0600

    Initial commit

---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from './assistant.response';
+export * from './audio-to-text.response';
+export * from './orthography.response';
+export * from './pros-cons.response';
+export * from './translate.response';
